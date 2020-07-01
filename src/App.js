a3e942fbd75d18e3857e40232ba6843cc394d07c
Andriy Oblivantsev <eslider@gmail.com>
2020-07-01 20:16:14 +0200

    Add simple eyewear search

--- src/App.js
+++ src/App.js
@@ -15,9 +15,11 @@ export default class App extends React.Component {
     // see ../config-overrides.js
 
     const me = this;
-    Promise.all([initSqlJs(), fetch('./test.db')]).then(async res => {
+    Promise.all([initSqlJs(), fetch('articles.sqlite')]).then(async res => {
       const SQLite = res[0], dbStorage = res[1];
       const db = new SQLite.Database(new Uint8Array(await dbStorage.arrayBuffer()));
+
+      setTimeout(() => this.search('Damen'))
 
       me.setState({db: db});
     }).catch(err => {
@@ -26,8 +28,35 @@ export default class App extends React.Component {
 
   }
 
-  exec(sql) {
+  search(query) {
     let results = null, err = null;
+    const sql = "SELECT t.* FROM db_articles t " +
+                "WHERE internal_article_id LIKE '%"+query+"%' " +
+                "OR ean_code LIKE  '%"+query+"%' " +
+                // "OR company_id LIKE  '%"+query+"%' " +
+                "OR brand LIKE  '%"+query+"%' " +
+                "OR user_group LIKE  '%"+query+"%' " +
+                "OR article_type LIKE  '%"+query+"%' " +
+                "OR line LIKE  '%"+query+"%' " +
+                "OR frame_dbl LIKE  '%"+query+"%' " +
+                "OR frame_rim_type LIKE  '%"+query+"%' " +
+                "OR frame_rx LIKE  '%"+query+"%' " +
+                "OR frame_colour LIKE  '%"+query+"%' " +
+                "OR frame_material LIKE  '%"+query+"%' " +
+                "OR frame_material_type LIKE  '%"+query+"%' " +
+                "OR frame_height LIKE  '%"+query+"%' " +
+                "OR frame_width LIKE  '%"+query+"%' " +
+                "OR frame_spring_hinge LIKE  '%"+query+"%' " +
+                "OR frame_temple_length LIKE  '%"+query+"%' " +
+                "OR frame_shape_type LIKE  '%"+query+"%' " +
+                "OR frame_shape_height LIKE  '%"+query+"%' " +
+                "OR frame_shape_length LIKE  '%"+query+"%' " +
+                "OR lens_material_type LIKE  '%" + query + "%' "
+                + "OR lens_colour LIKE  '%" + query + "%' "
+                // + "OR image_medium_url LIKE  '%" + query + "%' "
+                // + "OR delivery_range_index LIKE  '%" + query + "%' "
+                + "LIMIT 100;  "
+
     try {
       // The sql is executed synchronously on the UI thread. 
       // You may want to use a web worker
@@ -72,12 +101,11 @@ export default class App extends React.Component {
     return (
       <div className="App">
 
-        <h1>React SQL interpreter</h1>
+        <h1>Search eyewear</h1>
 
         <textarea
-          onChange={e => this.exec(e.target.value)}
-          placeholder="Enter some SQL. No inpiration ? Try “select sqlite_version()”"
-          defaultValue="SELECT * FROM db_articles"
+          onChange={e => this.search(e.target.value)}
+          defaultValue="Damen"
         />
 
         <pre className="error">{(err || "").toString()}</pre>
